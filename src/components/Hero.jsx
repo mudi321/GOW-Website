586626a74acc6b40777e566665e7b8ebaee52b71
Muditha Kalansooriya <mudi8833@gmail.com>
2025-02-05 23:54:27 +0530

    Remove loader

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -93,7 +93,7 @@ const Hero = () => {
   return (
     <div id='Hero' className='relative h-dvh w-screen overflow-x-hidden bg-bloodRed'>
 
-      {isLoading && (
+      {/* {isLoading && (
         <div className='flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-gold'>
           <div className='three-body'>
             <div className='three-body__dot'/>
@@ -101,7 +101,7 @@ const Hero = () => {
             <div className='three-body__dot'/>
           </div>
         </div>
-      )}
+      )} */}
 
         <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-light'>
             <div>
